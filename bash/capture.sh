d8a74bc87872702d1a8f9b7f5ffa2e4759d3380a
Chad Lee <git@chadly.net>
2015-05-25 21:58:12 -0500

    Capture all the streams

--- bash/capture.sh
+++ bash/capture.sh
@@ -12,4 +12,40 @@ avconv \
 	-segment_time 60 \
 	-segment_format mp4 \
 	-b:a 32k \
-	~/cams/`date +%s`-%03d.mp4
+	~/cams/living-room-`date +%s`-%03d.mp4 & \
+
+avconv \
+	-i rtsp://admin:Wodj0X9DBlYY@fycam.chadly.net:90/videoMain \
+	-vcodec copy \
+	-acodec aac \
+	-strict experimental \
+	-map 0 \
+	-f segment \
+	-segment_time 60 \
+	-segment_format mp4 \
+	-b:a 32k \
+	~/cams/front-yard-`date +%s`-%03d.mp4 & \
+
+avconv \
+	-i rtsp://admin:bt3tD8y4z8S3@fdcam.chadly.net:88/videoMain \
+	-vcodec copy \
+	-acodec aac \
+	-strict experimental \
+	-map 0 \
+	-f segment \
+	-segment_time 60 \
+	-segment_format mp4 \
+	-b:a 32k \
+	~/cams/front-door-`date +%s`-%03d.mp4 & \
+
+avconv \
+	-i rtsp://admin:tJ2ju5alBTUR@bycam.chadly.net:91/videoMain \
+	-vcodec copy \
+	-acodec aac \
+	-strict experimental \
+	-map 0 \
+	-f segment \
+	-segment_time 60 \
+	-segment_format mp4 \
+	-b:a 32k \
+	~/cams/backyard-`date +%s`-%03d.mp4
