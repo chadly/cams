0f716c395f0ca2076a96fe8363c4c4c7305cffa5
Chad Lee <git@chadly.net>
2015-05-30 13:49:45 -0500

    Change to directory of script

--- bash/native/process.sh
+++ bash/native/process.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#http://stackoverflow.com/a/6673798/316108
+mydir=$(dirname "$0") && cd "${mydir}" || exit 1
+
 ./rename.sh FI9821W_00626E52CB82 front-door
 ./rename.sh FI9821W_00626E52CB67 living-room
 ./rename.sh FI9805EP_00626E537A4F backyard
