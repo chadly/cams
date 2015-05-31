c0b7adc84061946ab77f4b269f7f2b51371afca7
Chad Lee <git@chadly.net>
2015-05-31 17:16:54 -0500

    Tweaking thumbnail generation script

--- bash/generate-preview.sh
+++ bash/generate-preview.sh
@@ -2,16 +2,19 @@
 
 folder=$1
 
-cd $folder
-
 thumbDir=`mktemp -d`
 
 #http://unix.stackexchange.com/a/63820/50868
 
-for i in *.mp4; do
-	avconv -i "$i" -r 1/10 -vf scale=-1:240 -vcodec png "$thumbDir/$i-%002d.png";
+for i in $folder/*.mp4; do
+	filename=$(basename $i)
+
+	#-ss 00:00:05 seek to 5 seconds in (when motion was detected)
+	#-vframes 1 capture 1 frame of the video (https://trac.ffmpeg.org/wiki/Create%20a%20thumbnail%20image%20every%20X%20seconds%20of%20the%20video)
+	#-vf scale=-1:240 scale to 240px height (https://trac.ffmpeg.org/wiki/Scaling%20%28resizing%29%20with%20ffmpeg)
+	avconv -i "$i" -ss 00:00:05 -vframes 1 -vf scale=-1:240 "$thumbDir/$filename-%002d.png";
 done
 
-montage -geometry +4+4 -tile 4x $thumbDir/*.png preview.png
+montage -geometry +1+1 -tile 4x $thumbDir/*.png $folder/00-00-00-preview.png
 
 rm -rf $thumbDir
