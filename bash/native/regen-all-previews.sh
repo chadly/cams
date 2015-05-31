0256a0b96f180d861b4c92a18113d729ad8f098f
Chad Lee <git@chadly.net>
2015-05-31 11:22:27 -0500

    fixing path on regen-all-previews

--- bash/native/regen-all-previews.sh
+++ bash/native/regen-all-previews.sh
@@ -6,7 +6,7 @@ mydir=$(dirname "$0") && cd "${mydir}" || exit 1
 #http://unix.stackexchange.com/a/84859/50868
 shopt -s nullglob
 
-for path in ~/cams/* ; do
+for path in ~/processed/* ; do
 	for datePath in $path/* ; do
 		./generate-preview.sh $datePath
 	done
