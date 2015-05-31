5db7ad37587f1bfa40a6c73a68a86557d568aa2e
Chad Lee <git@chadly.net>
2015-05-31 11:35:46 -0500

    Use temporary directory for thumbnail previews

--- bash/generate-preview.sh
+++ bash/generate-preview.sh
@@ -3,14 +3,15 @@
 folder=$1
 
 cd $folder
-mkdir -p thumbs
+
+thumbDir=`mktemp -d`
 
 #http://unix.stackexchange.com/a/63820/50868
 
 for i in *.mp4; do
-	avconv -i "$i" -r 1/10 -vf scale=-1:240 -vcodec png "thumbs/$i-%002d.png";
+	avconv -i "$i" -r 1/10 -vf scale=-1:240 -vcodec png "$thumbDir/$i-%002d.png";
 done
 
 montage -geometry +4+4 -tile 4x thumbs/*.png preview.png
 
-rm -rf thumbs
+rm -rf $thumbDir
