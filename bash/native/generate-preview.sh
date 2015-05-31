ed2df9d4f864a3c954e1da8e9378db07c0f0cc29
Chad Lee <git@chadly.net>
2015-05-31 11:18:24 -0500

    4 column preview instead of 6

--- bash/native/generate-preview.sh
+++ bash/native/generate-preview.sh
@@ -11,6 +11,6 @@ for i in *.mp4; do
 	avconv -i "$i" -r 1/10 -vf scale=-1:240 -vcodec png "thumbs/$i-%002d.png";
 done
 
-montage -geometry +4+4 -tile 6x thumbs/*.png preview.png
+montage -geometry +4+4 -tile 4x thumbs/*.png preview.png
 
 rm -rf thumbs
