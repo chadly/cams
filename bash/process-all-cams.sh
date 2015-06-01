ec5b572af9c3b95bc03c42330374eac591c42958
Chad Lee <git@chadly.net>
2015-05-31 19:58:06 -0500

    Fix path to script

--- bash/process-all-cams.sh
+++ bash/process-all-cams.sh
@@ -3,7 +3,7 @@
 #http://stackoverflow.com/a/6673798/316108
 mydir=$(dirname "$0") && cd "${mydir}" || exit 1
 
-./rename.sh FI9821W_00626E52CB82 front-door
-./rename.sh FI9821W_00626E52CB67 living-room
-./rename.sh FI9805EP_00626E537A4F backyard
-./rename.sh FI9805E_C4D65535806E front-yard
+./process.sh FI9821W_00626E52CB82 front-door
+./process.sh FI9821W_00626E52CB67 living-room
+./process.sh FI9805EP_00626E537A4F backyard
+./process.sh FI9805E_C4D65535806E front-yard
