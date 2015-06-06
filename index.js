1b26fc55ce95aa43151f010d2eb2c7c886650ef3
Chad Lee <git@chadly.net>
2015-06-06 15:29:24 -0500

    Pruning unused functions on server

--- index.js
+++ index.js
@@ -5,18 +5,6 @@ var cfg = require("./config");
 var fs = require("fs");
 var _ = require("lodash");
 
-if (!String.prototype.endsWith) {
-	String.prototype.endsWith = function(searchString, position) {
-		var subjectString = this.toString();
-		if (position === undefined || position > subjectString.length) {
-			position = subjectString.length;
-		}
-		position -= searchString.length;
-		var lastIndex = subjectString.indexOf(searchString, position);
-		return lastIndex !== -1 && lastIndex === position;
-	};
-}
-
 var app = express();
 
 app.engine('.hbs', exphbs({
@@ -90,29 +78,6 @@ app.get("/cameras/:id/:date", function(req, res) {
 
 		res.json(_.extend({}, cam, videos));
 	});
-});
-
-app.get("/c", function(req, res) {
-	if (req.body.username == cfg.username && req.body.password == cfg.password) {
-		fs.readdir("/home/chad/cams/front-yard/2015-06-06/", function(err, files) {
-			if (err) throw err;
-
-			var theVideos = [];
-			files.forEach(function(file) {
-				if (file.endsWith(".mp4")) {
-					theVideos.push("/recordings/front-yard/2015-06-06/" + file);
-				}
-			});
-
-			res.render("recordings", {
-				recordings: theVideos
-			});
-		});
-	} else {
-		res.render("login", {
-			authError: true
-		});
-	}
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
