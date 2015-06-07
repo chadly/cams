b16fbe5f6067c34da86b38852e2eb80a45497141
Chad Lee <git@chadly.net>
2015-06-06 21:48:23 -0500

    Don't generate .mp4.png files

--- bash/generate-thumbnails.sh
+++ bash/generate-thumbnails.sh
@@ -6,6 +6,7 @@ folder=$1
 
 for i in $folder/*.mp4; do
 	filename=$(basename $i)
+	filename="${filename%.*}"
 
 	#-ss 00:00:05 seek to 5 seconds in (when motion was detected)
 	#-vframes 1 capture 1 frame of the video (https://trac.ffmpeg.org/wiki/Create%20a%20thumbnail%20image%20every%20X%20seconds%20of%20the%20video)
