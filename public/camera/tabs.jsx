447a8364fcfa1a0df5f7f3f83fd82543466dd617
Chad Lee <git@chadly.net>
2015-06-07 11:07:48 -0500

    Selected tabs

--- public/camera/tabs.jsx
+++ public/camera/tabs.jsx
@@ -2,18 +2,21 @@ var React = require("react");
 
 var CameraTabs = React.createClass({
 	propTypes: {
-		cameras: React.PropTypes.array.isRequired
+		cameras: React.PropTypes.array.isRequired,
+		selected: React.PropTypes.string
 	},
 	render: function() {
 		return (
 			<ul className="nav nav-tabs">
 				{this.props.cameras.map(function(cam) {
+					var isActive = this.props.selected == cam.id ? "active" : "";
+
 					return (
-						<li key={cam.id} role="presentation">
+						<li key={cam.id} className={isActive} role="presentation">
 							<a href={'/' + cam.id + '/'}>{cam.name}</a>
 						</li>
 					);
-				})}
+				}.bind(this))}
 			</ul>
 		);
 	}
