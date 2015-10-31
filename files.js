ac1e4c8c608deeff530b51cefb195d0b92d3f722
Chad Lee <git@chadly.net>
2015-10-31 09:22:26 -0500

    Cache results of file lookups

--- files.js
+++ files.js
@@ -2,8 +2,14 @@ import fs from "fs";
 import path from "path";
 import Promise from "promise";
 import moment from "moment";
+import Cache from "node-cache";
 
 const readdir = Promise.denodeify(fs.readdir);
+const cache = new Cache({
+	stdTTL: 43200, //12 hours
+	checkPeriod: 3600, //1 hour
+	useClones: false //saving promises to cache
+});
 
 export default class FileReader {
 	constructor(baseDir) {
@@ -11,53 +17,76 @@ export default class FileReader {
 	}
 
 	readCameras() {
-		return readdir(this.baseDir);
+		var value = cache.get("cameras");
+		if (value === undefined) {
+			value = readdir(this.baseDir);
+			cache.set("cameras", value);
+		}
+
+		return value;
 	}
 
 	readDates(camera) {
-		var self = this;
+		const self = this;
+		const cacheKey = camera + "_dates";
 
-		return readdir(path.join(this.baseDir, camera)).then(function(files) {
-			return Promise.all(files.map(function(file) {
-				return new Promise(function(resolve, reject) {
-					self.readVideos(camera, file).then(function(files) {
-						resolve({
-							date: file,
-							files: files
-						});
-					}, reject);
+		var value = cache.get(cacheKey);
+		if (value === undefined) {
+			value = readdir(path.join(this.baseDir, camera)).then(function(files) {
+				return Promise.all(files.map(function(file) {
+					return new Promise(function(resolve, reject) {
+						self.readVideos(camera, file).then(function(files) {
+							resolve({
+								date: file,
+								files: files
+							});
+						}, reject);
+					});
+				}));
+			}).then(function(dates) {
+				return dates.map(function(date) {
+					return {
+						date: date.date,
+						videoCount: date.files.length
+					};
 				});
-			}));
-		}).then(function(dates) {
-			return dates.map(function(date) {
-				return {
-					date: date.date,
-					videoCount: date.files.length
-				};
 			});
-		});
+
+			cache.set(cacheKey, value);
+		}
+
+		return value;
 	}
 
 	readVideos(camera, date) {
-		return readdir(path.join(this.baseDir, camera, date)).then(function(files) {
-			var videos = [];
-			files.forEach(function(file) {
-				if (file.endsWith(".mp4")) {
-					videos.push(file);
-				}
-			});
-			return videos;
-		}).then(function(videos) {
-			return videos.map(function(file) {
-				var timeString = date + " " + file.replace(/.mp4/, "").replace(/-/g, ":");
-				var time = moment(timeString, "YYYY-MM-DD HH:mm:ss");
+		const cacheKey = camera + date + "_videos";
 
-				return {
-					time: time,
-					url: "/recordings/" + camera + "/" + date + "/" + file,
-					thumbnail: "/recordings/" + camera + "/" + date + "/" + file.replace(/mp4/, "png"),
-				};
+		var value = cache.get(cacheKey);
+		if (value === undefined) {
+			value = readdir(path.join(this.baseDir, camera, date)).then(function(files) {
+				var videos = [];
+				files.forEach(function(file) {
+					if (file.endsWith(".mp4")) {
+						videos.push(file);
+					}
+				});
+				return videos;
+			}).then(function(videos) {
+				return videos.map(function(file) {
+					var timeString = date + " " + file.replace(/.mp4/, "").replace(/-/g, ":");
+					var time = moment(timeString, "YYYY-MM-DD HH:mm:ss");
+
+					return {
+						time: time,
+						url: "/recordings/" + camera + "/" + date + "/" + file,
+						thumbnail: "/recordings/" + camera + "/" + date + "/" + file.replace(/mp4/, "png"),
+					};
+				});
 			});
-		});
+
+			cache.set(cacheKey, value);
+		}
+
+		return value;
 	}
 }
