2a992fd62f5f8d6f9645813afcc286abe70feb1b
Chad Lee <git@chadly.net>
2015-11-29 18:47:43 -0600

    Add kitchen processing

--- bash/process-all-cams.sh
+++ bash/process-all-cams.sh
@@ -7,5 +7,6 @@ mydir=$(dirname "$0") && cd "${mydir}" || exit 1
 ./process.sh FI9805EP_00626E537967 driveway
 ./process.sh FI9805EP_00626E537A4F backyard
 ./process.sh FI9805EP_C4D6553B70B1 backdoor
+./process.sh FI9821P_00626E58BD75 kitchen
 ./process.sh FI9821W_00626E52CB67 living-room
 ./process.sh FI9821W_00626E52CB82 front-door
