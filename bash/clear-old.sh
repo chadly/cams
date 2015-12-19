b1a723acc0288d4be782bd927e2c88e13d9beb09
Chad Lee <git@chadly.net>
2015-12-19 15:45:34 -0600

    Tweaks to clear-old-vidoes script

--- bash/clear-old.sh
+++ bash/clear-old.sh
@@ -1,10 +1,18 @@
-threshold=`date -d "90 days ago" +%Y-%m-%d`
+#!/bin/bash
+
+#http://unix.stackexchange.com/a/84859/50868
+shopt -s nullglob
+
+# http://stackoverflow.com/q/11448885
+threshold=`date -d "90 days ago" +%Y%m%d`
 
 for cam in ~/processed
 do
 	for vidDate in [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]
 	do
-		if test "$vidDate" -lt "$threshold"
+		vidNum=`echo "$vidDate" | tr -d -`   # remove dashes
+
+		if test "$vidNum" -lt "$threshold"
 		then
 			echo $vidDate
 		fi
